67c02121ebb8ea31745d362a49ffa8e9b5d8190c
Brett <nycbjr@gmail.com>
2014-05-02 07:29:52 -0400

    0502140729 - MergeBot - Merging packages/apps/Contacts from upstream sources

--- src/com/android/contacts/widget/TransitionAnimationView.java
+++ src/com/android/contacts/widget/TransitionAnimationView.java
@@ -49,7 +49,7 @@ public class TransitionAnimationView extends FrameLayout {
         mMaskingView.setVisibility(View.INVISIBLE);
         mMaskingView.setLayoutParams(new FrameLayout.LayoutParams(LayoutParams.MATCH_PARENT,
                 LayoutParams.MATCH_PARENT));
-        mMaskingView.setBackgroundColor(Color.BLACK);
+        mMaskingView.setBackgroundColor(Color.WHITE);
         addView(mMaskingView);
     }
 
